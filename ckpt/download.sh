af1fc840b00e5f80701b8ecb7e088f1886e3a832
Research-Lab <Research-Lab@DTJI0045.jioinstitute.edu.in>
2025-06-30 13:43:10 +0530

    first commit

---
+++ ckpt/download.sh
@@ -0,0 +1,6 @@
+# download from huggingface face, takes 20G space
+git lfs install
+
+git clone https://huggingface.co/runwayml/stable-diffusion-v1-5
+git clone https://huggingface.co/chenyangqi/jeep_tuned_200
+git clone https://huggingface.co/chenyangqi/swan_150
